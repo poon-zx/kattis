d6cc8cf78c6dc50911765c9919e3f7ddcb6679a2
poon-zx <poonzhexuan@gmail.com>
2025-02-04 00:47:48 +0800

    Create magictrick.cpp

---
+++ magictrick.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using i64 = long long;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr std::array<std::array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+void solve() {
+  std::string str;
+  std::cin >> str;
+  std::unordered_set<char> set;
+  for (auto &ch : str) {
+    if (set.find(ch) != set.end()) {
+        std::cout << 0;
+        return;
+    } else {
+        set.insert(ch);
+    }
+  }
+  std::cout << 1;
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
