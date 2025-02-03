6bf0417f9e6844d592a79c02e14dcf503c948175
poon-zx <poonzhexuan@gmail.com>
2025-02-04 00:23:57 +0800

    Initial commit

---
+++ speeding.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using i64 = long long;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr std::array<std::array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+void solve() {
+  int N;
+	std::cin >> N;
+	int res = 0;
+	int a, b;
+	std::cin >> a >> b;
+	int c, d;
+	for (int i = 1; i < N; i++) {
+		std::cin >> c >> d;
+		res = std::max(res, (d - b) / (c - a));
+		a = c;
+		b = d;
+	}
+	std::cout << res;
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
