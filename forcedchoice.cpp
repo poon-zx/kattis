6cfc3d8e56f0c381e3d34968f839ff7299e24187
poon-zx <poonzhexuan@gmail.com>
2025-02-04 09:01:39 +0800

    Create forcedchoice.cpp

---
+++ forcedchoice.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using i64 = long long;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr std::array<std::array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+void solve() {
+  int total, correct, cards;
+  std::cin >> total >> correct >> cards;
+
+  for (int i = 0; i < cards; i++) {
+    bool have = false;
+    int c, x;
+    std::cin >> c;
+    for (int j = 0; j < c; j++) {
+      std::cin >> x;
+      if (x == correct) {
+        have = true;
+      }
+    }
+    if (have) {
+      std::cout << "KEEP" << "\n";
+    } else {
+      std::cout << "REMOVE" << "\n";
+    }
+  }
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
