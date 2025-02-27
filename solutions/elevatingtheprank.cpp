ef4e6a0ed7260975459bd99d5c7fc536f0bb1b38
poon-zx <poonzhexuan@gmail.com>
2025-02-28 01:17:12 +0800

    27 Feb: 405.6

---
+++ solutions/elevatingtheprank.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+using ld = long double;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr array<array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+// vector<vector<int>> v(3, vector<int>(4,0) 3x4 filled with 0s
+
+void solve() {
+  int start,desti;
+  cin>>start>>desti;
+  int n;
+  cin>>n;
+  int res=abs(desti-start)*4;
+  unordered_set<int> s;
+  for (int i=0;i<n;i++) {
+    int t;
+    cin>>t;
+    if (s.find(t)!=s.end()) {continue;}
+    s.insert(t);
+    if ((start<desti&&t<desti&&t>start)||(start>desti&&t<start&&t>desti)) {res+=10;}
+  }
+  cout<<res;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
---
+++ solutions/gameofthrowns.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+using ld = long double;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr array<array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+// vector<vector<int>> v(3, vector<int>(4,0) 3x4 filled with 0s
+
+void solve() {
+  int n,k;
+  cin>>n>>k;
+  stack<int> st;
+  for (int i=0;i<k;i++) {
+    string s;
+    cin>>s;
+    if (s=="undo") {
+      int num;
+      cin>>num;
+      while (num--) {
+        st.pop();
+      }
+    } else {
+      st.push(stoi(s));
+    }
+  }
+  ll res=0;
+  while (!st.empty()) {
+    res+=st.top();
+    st.pop();
+  }
+  if (res>0) {
+    res%=n;
+  } else if (res<0) {
+    res = ((res % n) + n) % n;
+  }
+  cout<<res;
+
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
