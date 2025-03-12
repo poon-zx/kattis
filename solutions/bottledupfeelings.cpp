f735fc2133022cafef0ebfbfb34006c1fc9e87e9
poon-zx <poonzhexuan@gmail.com>
2025-03-12 19:59:16 +0800

    12 Mar: 548.5

---
+++ solutions/bottledupfeelings.cpp
@@ -0,0 +1,73 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+using ld = long double;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr array<array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+// vector<vector<int>> v(3, vector<int>(4,0) 3x4 filled with 0s
+
+void solve() {
+  int s,v1,v2;
+  cin>>s>>v1>>v2;
+  int start_big=s/v1;
+  int diff,small;
+  while (start_big>=0) {
+    diff=s-start_big*v1;
+    small=diff/v2;
+    //cout<<small<<" "<<diff<<"\n";
+    if (small*v2==diff) break;
+    start_big--;
+  }
+  if (start_big<0) cout<<"Impossible";
+  else cout<<start_big<<" "<<small;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
---
+++ solutions/eventplanning.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+using ld = long double;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr array<array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+// vector<vector<int>> v(3, vector<int>(4,0) 3x4 filled with 0s
+
+void solve() {
+  int p,b,h,w;
+  cin>>p>>b>>h>>w;
+  ll minn=numeric_limits<ll>::max();
+  for (int i=0;i<h;i++) {
+    ll price;
+    cin>>price;
+    for (int i=0;i<w;i++) {
+      int bed;
+      cin>>bed;
+      if (bed<p||price*(ll)p>(ll)b) continue;
+      minn=min(minn,price*(ll)p);
+    }
+  }
+  if (minn==numeric_limits<ll>::max()) cout<<"stay home";
+  else cout<<minn;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
