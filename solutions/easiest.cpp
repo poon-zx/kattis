75f8adca857f1821175f8d1f2065b1c10f9483b3
poon-zx <poonzhexuan@gmail.com>
2025-03-17 21:22:30 +0800

    17 Mar: 597.0

---
+++ solutions/easiest.cpp
@@ -0,0 +1,82 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+using ld = long double;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr array<array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+// vector<vector<int>> v(3, vector<int>(4,0) 3x4 filled with 0s
+
+ll digits_sum(ll num) {
+  ll summ=0;
+  while (num>0) {
+    summ+=num%10;
+    num/=10;
+  }
+  return summ;
+};
+
+void solve() {
+  ll num;
+  while (cin>>num&&num!=0) {
+    ll summ=digits_sum(num);
+    ll p=11;
+    while (true) {
+      ll prod=p*num;
+      if (digits_sum(prod)==summ) {
+        cout<<p<<"\n";
+        break;
+      }
+      p++;
+    }
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
---
+++ solutions/prsteni.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+using ld = long double;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr array<array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+// vector<vector<int>> v(3, vector<int>(4,0) 3x4 filled with 0s
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+};
+
+void solve() {
+  int n;
+  cin>>n;
+  vector<int> v(n);
+  for (int i=0;i<n;i++) cin>>v[i];
+  for (int i=1;i<n;i++) {
+    int g=gcd(v[i],v[0]);
+    cout<<v[0]/g<<"/"<<v[i]/g<<"\n";
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
