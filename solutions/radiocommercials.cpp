61c5e7c88bfa014230c38c7cdde54504c67ebfc5
poon-zx <poonzhexuan@gmail.com>
2025-03-21 22:37:49 +0800

    21 Mar: 642.6

---
+++ solutions/radiocommercials.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+using ld = long double;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr array<array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+// vector<vector<int>> v(3, vector<int>(4,0) 3x4 filled with 0s
+
+void solve() {
+  ll n,p;
+  cin>>n>>p;
+  ll res=0,now=0;
+  ll t;
+  for (ll i=0;i<n;i++) {
+    cin>>t;
+    t-=p;
+    now=max((ll)0,now);
+    now+=t;
+    res=max(res,now);
+  }
+  cout<<res;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
---
+++ solutions/sretan.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+using ld = long double;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr array<array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+// vector<vector<int>> v(3, vector<int>(4,0) 3x4 filled with 0s
+
+void solve() {
+  ll n;
+  cin>>n;
+  ll l=2,power=1;
+  ll cum=0;
+  while (cum+pow(l,power)<n) {
+    cum+=pow(l,power);
+    power++;
+  }
+  ll offset=n-1-cum;
+  vector<int> v;
+  for (ll i=0;i<power;i++) {
+    if (offset&(1LL<<(power-1-i))) v.push_back(7);
+    else v.push_back(4);
+  }
+  for (auto num:v) cout<<num;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
