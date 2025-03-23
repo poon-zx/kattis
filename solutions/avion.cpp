1e4d5fcaf95475a8bd0d3cfe8eb698bde397a129
poon-zx <poonzhexuan@gmail.com>
2025-03-24 00:32:20 +0800

    24 Mar: 670.9

---
+++ solutions/avion.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+using ld = long double;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr array<array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+// vector<vector<int>> v(3, vector<int>(4,0) 3x4 filled with 0s
+
+void solve() {
+  vector<int> v;
+  for (int i=0;i<5;i++) {
+    string s;
+    cin>>s;
+    if (s.find("FBI")!=string::npos) {
+      v.push_back(i+1);
+    }
+  }
+  if (v.empty()) cout<<"HE GOT AWAY!";
+  else {
+    for (auto& i:v) {
+      cout<<i<<" ";
+    }
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
---
+++ solutions/fiftyshades.cpp
@@ -0,0 +1,73 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+using ld = long double;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr array<array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+// vector<vector<int>> v(3, vector<int>(4,0) 3x4 filled with 0s
+
+void solve() {
+  int n;
+  cin>>n;
+  int res=0;
+  for (int i=0;i<n;i++) {
+    string s;
+    cin>>s;
+    for (int j=0;j<s.size();j++) {
+      s[j]=tolower(s[j]);
+    }
+    if (s.find("pink")!=string::npos||s.find("rose")!=string::npos) res++;
+  }
+  if (res==0) cout<<"I must watch Star Wars with my daughter";
+  else cout<<res;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
