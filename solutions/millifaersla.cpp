3082b4ad9460855673b2246e77b73104257a427b
poon-zx <poonzhexuan@gmail.com>
2025-04-01 23:26:24 +0800

    1 Apr: 739.4

---
+++ solutions/millifaersla.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <ios>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+using ld = long double;
+
+constexpr int MOD = 1'000'000'007;
+
+constexpr array<array<int, 2>, 4> directions{{
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+}};
+
+// vector<vector<int>> v(3, vector<int>(4,0) 3x4 filled with 0s
+
+void solve() {
+  ll a,b,c;
+  cin>>a>>b>>c;
+  vector<ll> v={a,b,c};
+  sort(v.begin(),v.end());
+  if (v[0]==a) cout<<"Monnei";
+  else if (v[0]==b) cout<<"Fjee";
+  else cout<<"Dolladollabilljoll";
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int tc{1};
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
